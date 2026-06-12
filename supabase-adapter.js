b91b4f6d06c61a10fc192763801eeb9ff5b9680d
izankcruzz <izankcruzz@gmail.com>
2026-06-12 16:37:32 +0700

    Add files via upload

--- supabase-adapter.js
+++ supabase-adapter.js
@@ -1,5 +1,5 @@
 (function () {
-  window.POS_SUPABASE_ADAPTER_VERSION = "2026-06-12-mobile-icon-v62";
+  window.POS_SUPABASE_ADAPTER_VERSION = "2026-06-12-sales-total-label-v61";
   console.info("POS Supabase adapter", window.POS_SUPABASE_ADAPTER_VERSION);
 
   const STORAGE_URL = "POS_SUPABASE_URL";
