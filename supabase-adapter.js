fba52064aedb5145398b8a2b04dc2ac91410aca8
izankcruzz <izankcruzz@gmail.com>
2026-06-13 16:43:26 +0700

    Add files via upload

--- supabase-adapter.js
+++ supabase-adapter.js
@@ -1,5 +1,5 @@
 (function () {
-  window.POS_SUPABASE_ADAPTER_VERSION = "2026-06-13-sales-cost-at-sale-v70";
+  window.POS_SUPABASE_ADAPTER_VERSION = "2026-06-13-month-money-summary-top-v71";
   console.info("POS Supabase adapter", window.POS_SUPABASE_ADAPTER_VERSION);
 
   const STORAGE_URL = "POS_SUPABASE_URL";
